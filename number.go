a42e5b9e55830fd2222229aa8910663c5e92714b
Rendy <alifrendy663@gmail.com>
2025-04-04 00:25:13 +0700

    tipe data boolean

--- number.go
+++ number.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+// func main() {
+// 	fmt.Println("satu = ", 1)
+// 	fmt.Println("dua =", 2)
+// 	fmt.Println("tiga koma lima =", 3.5)
+// }
+
 func main() {
-	fmt.Println("satu = ", 1)
-	fmt.Println("dua =", 2)
-	fmt.Println("tiga koma lima =", 3.5)
+	fmt.Println("nol =", 0)
+	fmt.Println("negatif lima =", -5)
+	fmt.Println("seribu koma dua lima =", 1000.25)
+
+	fmt.Println("hasil dari 5 + 7 =", 5+7)
+
+	var x int = 10
+	var y float64 = 2.5
+	fmt.Println("Angka pertama =", x)
+	fmt.Println("Angka kedua =", y)
 }
